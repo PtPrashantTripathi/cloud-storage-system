9c87dd29463242015c97caf026cd6e9810ecb1df
ptprashanttripathi <pt8435782545@gmail.com>
2019-05-12 12:27:37 +0530

    test

--- Server Source Code/CloudBox/download.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$file_name = 'cloudbox.png';
-    $file_url = 'http://localhost/image.php?image_id='.$_GET['image_id'];
-    header('Content-Type: application/octet-stream');
-    header("Content-Transfer-Encoding: Binary"); 
-    header("Content-disposition: attachment; filename=\"".$file_name."\""); 
-    echo file_get_contents($file_url);
-?>
--- Server Source Code/CloudBox/include/session_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(!isset($_SESSION["id"])) {     
-	//$_SESSION["id"]='1';
-header("Location: /login.php?session_expired"); 
-}
-?>
